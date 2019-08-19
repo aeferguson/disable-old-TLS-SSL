f811c231f0beb379e30dd8522cfa398ce137b2c3
Andrew Ferguson <andrew.e.ferguson@icloud.com>
2019-08-19 11:34:46 -0500

    Update disable-old-TLS-SSL.ps1
    changed write-host to write-output

--- disable-old-TLS-SSL.ps1
+++ disable-old-TLS-SSL.ps1
@@ -5,12 +5,12 @@ $LogName = 'tls-versions.log';
 New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server' -Force | Out-File -FilePath $LogDir$LogName -Append
 New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server' -name Enabled -value '0' -PropertyType DWORD -Force | Out-File -FilePath $LogDir$LogName -Append
 
-Write-Host 'SSL 2.0 has been disabled.'
+Write-Output 'SSL 2.0 has been disabled.'
 
 New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server' -Force | Out-File -FilePath $LogDir$LogName -Append
 New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server' -name Enabled -value '0' -PropertyType DWORD -Force | Out-File -FilePath $LogDir$LogName -Append
 
-Write-Host 'SSL 3.0 has been disabled.'
+Write-Output 'SSL 3.0 has been disabled.'
 
 # These commands will disable TLS 1.0
 New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' -Force | Out-File -FilePath $LogDir$LogName -Append
@@ -21,7 +21,7 @@ New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Prot
 New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-File -FilePath $LogDir$LogName -Append
 New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-File -FilePath $LogDir$LogName -Append
 
-Write-Host 'TLS 1.0 has been disabled.'
+Write-Output 'TLS 1.0 has been disabled.'
 
 # These commands will disable TLS 1.1
 New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -Force | Out-File -FilePath $LogDir$LogName -Append
@@ -32,7 +32,7 @@ New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Prot
 New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-File -FilePath $LogDir$LogName -Append
 New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-File -FilePath $LogDir$LogName -Append
 
-Write-Host 'TLS 1.1 has been disabled.'
+Write-Output 'TLS 1.1 has been disabled.'
 
 # Add and Enable TLS 1.2 for client and server SCHANNEL communications
 New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server' -Force | Out-File -FilePath $LogDir$LogName -Append
@@ -43,6 +43,6 @@ New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Prot
 New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client' -name 'Enabled' -value 1 -PropertyType 'DWord' -Force | Out-File -FilePath $LogDir$LogName -Append
 New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client' -name 'DisabledByDefault' -value 0 -PropertyType 'DWord' -Force | Out-File -FilePath $LogDir$LogName -Append
 
-Write-Host "TLS 1.2 protocol has been enabled."
+Write-Output "TLS 1.2 protocol has been enabled."
 
-Write-Host 'Please reboot your machine to apply settings'
+Write-Output 'Please reboot your machine to apply settings'
